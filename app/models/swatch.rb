3fb5428dec18894be4d91bb1828352c1bdb67eb8
Harris Novick <harris@harrisnovick.com>
2013-02-17 04:38:24 -0500

    setting up sanitizer

--- app/models/swatch.rb
+++ app/models/swatch.rb
@@ -3,4 +3,11 @@ class Swatch < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :name, :description, :markup
+
+  class << self
+
+    def sanitize_markup(markup)
+      markup
+    end
+  end
 end
