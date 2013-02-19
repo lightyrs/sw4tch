c740e543ed559ba1543fb8308fc0cdd818d31b5e
Harris Novick <harris@harrisnovick.com>
2013-02-19 04:36:26 -0500

    convert from/to stylus and scss

--- app/models/swatch.rb
+++ app/models/swatch.rb
@@ -12,8 +12,6 @@ class Swatch < ActiveRecord::Base
   validates :stylus, presence: true
 
   after_initialize  :assign_markup
-  before_validation :set_markup
-
 
   def assign_markup
     %w(css scss stylus).each do |attr|
@@ -21,28 +19,32 @@ class Swatch < ActiveRecord::Base
     end
   end
 
-  def set_markup
-
-  end
-
-  def convert_string(from, to)
+  def convert_markup(from, to)
     instance_eval("#{from}_to_#{to}")
   end
 
-  def css_to_scss
-    ::Sass::CSS.new(css).render(:scss)
+  def css_to_scss(_css=nil)
+    ::Sass::CSS.new(_css||css).render(:scss)
   end
 
-  def css_to_stylus
-    Stylus.convert(css)
+  def css_to_stylus(_css=nil)
+    Stylus.convert(_css||css)
   end
 
   def scss_to_css
     Sass::Engine.new(scss, syntax: :scss).to_css
   end
 
+  def scss_to_stylus
+    css_to_stylus scss_to_css
+  end
+
   def stylus_to_css
     Stylus.compile(stylus)
+  end
+
+  def stylus_to_scss
+    css_to_scss stylus_to_css
   end
 
   def default_css
@@ -68,6 +70,7 @@ class Swatch < ActiveRecord::Base
   def default_scss
     <<-eos.strip_heredoc
       .swatch {
+        display: block;
 
         .specimen {
           display: block;
@@ -87,15 +90,16 @@ class Swatch < ActiveRecord::Base
   def default_stylus
     <<-eos.strip_heredoc
       .swatch
+        display block
 
         .specimen
           display block
 
           &:before
-            content: ''
+            content ''
 
           &:after
-            content: ''
+            content ''
     eos
   end
 end
