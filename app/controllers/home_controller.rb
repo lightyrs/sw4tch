f117dcdd6aaea8c910daa5e890f2273ef689c6a1
Harris Novick <harris@harrisnovick.com>
2013-03-20 02:10:29 -0400

    Add swatchbooks to homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @swatches = Swatch.order('created_at DESC').paginate(page: params[:page], per_page: 36)
+    @swatchbooks = Swatchbook.order('created_at DESC').paginate(page: params[:page], per_page: 10)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id]) || current_user
-    @swatches = @user.swatches.paginate(page: params[:page], per_page: 36)
-    @swatchbooks = @user.swatchbooks.paginate(page: params[:page], per_page: 10)
+    @swatches = @user.swatches.order('created_at DESC').paginate(page: params[:page], per_page: 36)
+    @swatchbooks = @user.swatchbooks.order('created_at DESC').paginate(page: params[:page], per_page: 10)
   end
 end
