9f99d821ba55e232a8a3f0f333e52b1a8cf25384
Harris Novick <harris@lightyrs.com>
2013-03-21 18:36:51 -0300

    Undo

--- app/models/swatch.rb
+++ app/models/swatch.rb
@@ -67,6 +67,7 @@ class Swatch < ActiveRecord::Base
 
   def stylus_to_css(_stylus=nil)
     _stylus ||= stylus
+    _stylus = import_nib(_stylus)
     Stylus.compile("#{_stylus}")
   end
 
@@ -80,6 +81,10 @@ class Swatch < ActiveRecord::Base
 
   def import_compass(markup)
     "@import 'compass/css3';#{markup}"
+  end
+  
+  def import_nib(markup)
+    "@import 'nib';#{markup}"
   end
 
   def default_css
